f8ec5c30c67524acadcdba6852cd77f8834128b0
imranlielchin <elchinii@code.edu.az>
2022-12-07 19:10:41 +0400

    init

---
+++ Kamitom/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Kamitom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstNumber > 99) Console.WriteLine("Daxil Etdiyiniz Eded 3 Reqemlidir");
+            else Console.WriteLine("Daxil Etdiyiniz Reqem Yalnisdir");
+
+
+
+
+        }
+            
+        }
+    }
